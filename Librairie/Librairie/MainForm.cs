70363542901104638727782938cc10a9a6b9cdca
Arthur Magnette <amagnette@magicsoftware.com>
2024-01-18 09:00:22 +0100

    correction interface

--- Librairie/Librairie/MainForm.cs
+++ Librairie/Librairie/MainForm.cs
@@ -76,7 +76,7 @@ namespace Librairie
             dataGridView1.Columns[3].HeaderText = "Éditeur";
             dataGridView1.Columns[4].HeaderText = "Auteur";
             dataGridView1.Columns[5].HeaderText = "Catégorie";
-            dataGridView1.Columns[0].Width = 50;
+            dataGridView1.Columns[0].Width = 150;
             dataGridView1.Columns[1].Width = 200;
             dataGridView1.Columns[2].Width = 90;
             dataGridView1.Columns[3].Width = 120;
